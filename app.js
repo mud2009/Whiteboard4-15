b21620481107acf8a29529d63bbebbd906c30e58
Will Greenberg <greenberg.will@gmail.com>
2022-04-15 09:07:37 -0700

    solve second q

--- app.js
+++ app.js
@@ -30,39 +30,19 @@ function stringToUrlRecursive(string){
 
 // Output: [7, 9, "hi", 12, 53]
 
-// Question #3: Compressing Strings
-// Write an algorithm that takes a string with repeated characters and compresses them, using a number to show how many times the repeated character has been compressed. For instance, aaa would be written as 3a. Solve the problem with and without recursion.
+function dedupe(arr){
+  for(let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){
+      if (arr[i] === arr[j]){
+        arr.splice(j, 1)
+      }
+    }
+  }
+  return arr
+}
 
-// Example
-// Input: "aaabccdddda"
+// with filter
 
-// Output: "3ab2c4da"
-
-// Question #4: Checking for Uniqueness
-// Write an algorithm that determines whether all the elements in a string are unique. You may not convert the string into an array or use array methods to solve this problem. The algorithm should return a boolean.
-
-// Example
-// Input: "hello"
-
-// Output: false
-
-// Input: "copyright"
-
-// Output: true
-
-// Question #5: Array Sorting
-// Write an algorithm that sorts an array without using the sort() method. There are many different sorting algorithms - take the time to read about the following:
-
-// Quick sort
-// Merge sort
-// Heap sort
-// Insertion sort
-// Bubble sort
-// Selection sort
-// You may implement any of the above algorithms (or your own) to solve the problem - as long as it doesn't use sort().
-
-// Example
-// Input: [9, 2, 7, 12]
-
-// Output: [2, 7, 9, 12]
-
+function dedupeFilter(arr){
+  return arr.filter((x, i) => arr.indexOf(x) === i)
+}
