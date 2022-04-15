a67e81e07e1d8a64394a36042c6e983712f0a52f
Will Greenberg <greenberg.will@gmail.com>
2022-04-15 13:54:56 -0700

    solve q 3

--- app.js
+++ app.js
@@ -8,18 +8,18 @@
 
 // Output: "Jasmine%20Ann%20Jones"
 
-// function stringToUrl(string){
-//   return string.split(" ").join("%20")
-// }
+function stringToUrl(string){
+  return string.split(" ").join("%20")
+}
 
 // // with recursion
 
-// function stringToUrlRecursive(string){
-//   if (string === ""){
-//     return ""
-//   }
-//   return stringToUrlRecursive
-// }
+function stringToUrlRecursive(string){
+  if (string === ""){
+    return ""
+  }
+  return stringToUrlRecursive
+}
 
 
 // Question #2: Array Deduping
@@ -30,37 +30,36 @@
 
 // Output: [7, 9, "hi", 12, 53]
 
-// function dedupe(arr){
-//   for(let i = 0; i < arr.length; i++){
-//     for(let j = i + 1; j < arr.length; j++){
-//       if (arr[i] === arr[j]){
-//         arr.splice(j, 1)
-//       }
-//     }
-//   }
-//   return arr
-// }
+function dedupe(arr){
+  for(let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){
+      if (arr[i] === arr[j]){
+        arr.splice(j, 1)
+      }
+    }
+  }
+  return arr
+}
 
 // with set
 
-// function dedupeSet(arr){
-//   return [...new Set(arr)]
-// }
+function dedupeSet(arr){
+  return [...new Set(arr)]
+}
 
 // with filter
 
-// function dedupeFilter(arr){
-//   return arr.filter((x, i) => arr.indexOf(x) === i)
-// }
+function dedupeFilter(arr){
+  return arr.filter((x, i) => arr.indexOf(x) === i)
+}
 
 // with recursion
 
-// function dedupeRecursion(arr, newArr = []){
-//   if(arr === []){
-//     return "empty arr";
-//   }
-
-// }
+function dedupeRecursion(arr, newArr = []){
+  if(arr === []){
+    return "empty arr";
+  }
+}
 
 // Question #3: Compressing Strings
 // Write an algorithm that takes a string with repeated characters and compresses them, using a number to show how many times the repeated character has been compressed. For instance, aaa would be written as 3a. Solve the problem with and without recursion.
@@ -74,13 +73,12 @@ function compress(x){
   let arr = []
   let counter = 1
   for (let i = 0; i < x.length; i ++){
-    console.log(x[i] + " " +  x[i + 1])
     if(x[i] === x[i + 1]){
       counter ++
     }
     else {
       if (counter > 1){
-        arr.push(counter + x[i])
+        arr.push(counter)
       }
       arr.push(x[i])
       counter = 1
@@ -89,7 +87,8 @@ function compress(x){
   return arr.join("");
 }
 
-console.log(compress("aaabccdddda"))
+
+console.log(compressRec("aaabccdddda"))
 
 // Question #4: Checking for Uniqueness
 // Write an algorithm that determines whether all the elements in a string are unique. You may not convert the string into an array or use array methods to solve this problem. The algorithm should return a boolean.
@@ -103,18 +102,24 @@ console.log(compress("aaabccdddda"))
 
 // Output: true
 
-// function unique(string){
-//   for (let i = 0; i < string.length; i ++){
-//     for (let j = i + 1; j < string.length; j ++){
-//       if (string[i] === string[j]){
-//         return false;
-//       }
-//       else {
-//         return true;
-//       }
-//     }
-//   }
-// }
+function unique(string){
+  for (let i = 0; i < string.length; i ++){
+    for (let j = i + 1; j < string.length; j ++){
+      if (string[i] === string[j]){
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
+  }
+}
+
+function uniqueSet(string){
+  return ([...new Set(string)].join("") === string)
+}
+
+// console.log(uniqueSet("copyright"))
 
 // Question #5: Array Sorting
 // Write an algorithm that sorts an array without using the sort() method. There are many different sorting algorithms - take the time to read about the following:
@@ -132,17 +137,17 @@ console.log(compress("aaabccdddda"))
 
 // Output: [2, 7, 9, 12]
 
-// function selecSort(arr){
-//   for(let i = 0; i < arr.length; i++){
-//     for(let j = i + 1; j < arr.length; j++){
-//       if (arr[i] > arr[j]){
-//         let arrHigh = arr[i];
-//         arr[i] = arr[j];
-//         arr[j] = arrHigh;
-//       }
-//     }
-//   }
-//   return arr;
-// }
+function selecSort(arr){
+  for(let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){
+      if (arr[i] > arr[j]){
+        let arrHigh = arr[i];
+        arr[i] = arr[j];
+        arr[j] = arrHigh;
+      }
+    }
+  }
+  return arr;
+}
 
 // recursive sort
