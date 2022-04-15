95e1cff2e1ba30de7d4098deea4ba55cd55bd483
Will Greenberg <greenberg.will@gmail.com>
2022-04-15 10:48:14 -0700

    solve q 4

--- app.js
+++ app.js
@@ -64,11 +64,6 @@ function dedupeRecursion(arr){
 
 // Output: "3ab2c4da"
 
-function compress(string){
-  let arr = string.split("")
-  
-}
-
 // Question #4: Checking for Uniqueness
 // Write an algorithm that determines whether all the elements in a string are unique. You may not convert the string into an array or use array methods to solve this problem. The algorithm should return a boolean.
 
